2d1ddccc3f62581a590ea4bed4e75aacaa9606ab
Misliv <mayandmays@hotmail.com>
2024-09-13 02:11:30 +0200

    Main menu & double jump

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,15 +4,19 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -500.0
 
+var jumpsMade = 0
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else: jumpsMade = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() || jumpsMade < 2):
 		velocity.y = JUMP_VELOCITY
+		jumpsMade += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
